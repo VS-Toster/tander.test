1535500596bcd5684b6edcc70cfc139b8011cd31
VS-Toster <63507226+VS-Toster@users.noreply.github.com>
2020-10-07 00:18:21 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import service.MainService;
+
+import java.sql.*;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        MainService mainService = new MainService();
+        mainService.start(1000);
+
+    }
+}
+
+
+
+
